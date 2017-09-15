ae3faa97160b978bea9a40f4056f6e8bd78eadfa
terry <deng0981@sina.com>
2017-09-15 18:57:22 +0800

    inite DB exec

---
+++ positive.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+品正科技
+*/
+namespace positiveDB;
+use PDO;
+use Exception;
+use PDOException;
+
+class positiveDB
+{
+    public function _construct()
+    {
+        
+    }
+}
+
+
+?>
